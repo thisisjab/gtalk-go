c1597661fb5cdd0ee40deafb29d9f881c1311191
thisisjab <heythisisjab@gmail.com>
2025-05-01 23:22:44 +0330

    feat(api): return api version and env in healthcheck

--- api/healthcheck.go
+++ api/healthcheck.go
@@ -1,11 +1,18 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (s *APIServer) handleGetHealthCheck(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "OK")
+	err := s.writeJSON(w, http.StatusOK, envelope{"status": "available",
+		"system_info": map[string]string{
+			"environment": s.config.Environment,
+			"version":     s.config.Version,
+		},
+	}, nil)
+
+	if err != nil {
+		s.serverErrorResponse(w, r, err)
+	}
 }
